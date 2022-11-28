135611875db01acee803857f4e3e29f4c5647dfb
RastelliBru <bruna.rastelli@gmail.com>
2022-11-28 15:06:21 -0300

    alterando informações na função

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,8 +1,8 @@
 let contas = [];
-const data = new Date();
-let dataFormatada = data.toISOString().substring(0, 10);
 
 export function createUserUseCase(nome, email, senha) {
+  const data = new Date();
+  let dataFormatada = data.toISOString().substring(0, 10);
   const user = {
     id: contas.length + 1,
     name: nome,
