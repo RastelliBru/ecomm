e977850cc74e717849b1881c5ac42ea8c5fd06a2
RastelliBru <bruna.rastelli@gmail.com>
2022-12-15 22:30:53 -0300

    encriptando senhas

--- account/src/routes.js
+++ account/src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createUserUseCase } from "./use-case/createUserAccount.js";
+import bcrypt from "bcryptjs";
 
 export const router = Router();
 
 router.post("/account", function (req, res) {
   const { name, email, password } = req.body;
-  createUserUseCase(name, email, password)
+  const encodedPassword = bcrypt.hashSync(password, 10);
+  createUserUseCase(name, email, encodedPassword)
     .then((user) => {
       const userCreated = {
         name: user.name,
