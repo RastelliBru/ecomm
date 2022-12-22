efba7c73133e3956927ce04358adb28eb18787e5
RastelliBru <bruna.rastelli@gmail.com>
2022-12-22 19:26:51 -0300

    documentando API com swagger

--- account/src/main.js
+++ account/src/main.js
@@ -1,10 +1,22 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes.js";
+import apiDocs from './api-docs.json' assert {type: "json"};
 
 const app = express();
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
+
 app.use(express.json());
 app.use(router);
+
+app.get("/terms", (req, res) =>{
+  return res.json({
+    message: "termos de serviço"
+  })
+})
+
+app.use("/accountsV1", router)
 
 app.listen(3000, function () {
   console.log("running");
--- product/src/main.js
+++ product/src/main.js
@@ -1,10 +1,22 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./router.js";
+import swaggerDocs from "./api-docs.json" assert {type: "json"}
 
 const app = express();
 
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+
 app.use(express.json());
 app.use(router);
+
+
+app.get("/terms", (req, res) => {
+  return res.json ({
+    message: "termos de serviço"
+  })
+})
 
 app.listen(3001, function () {
   console.log("running");
