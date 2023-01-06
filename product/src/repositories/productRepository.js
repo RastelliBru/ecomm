6788fcc6b50d46ad5ba843390925bce314f3358c
RastelliBru <bruna.rastelli@gmail.com>
2023-01-06 12:02:17 -0300

    erro de conexão com o mysqlbd

--- product/src/repositories/productRepository.js
+++ product/src/repositories/productRepository.js
@@ -1,4 +1,25 @@
-const products = [];
+
+import { Sequelize }from 'sequelize'
+import express from 'express';
+
+
+
+const app = express();
+const sequelize = new Sequelize('mysql://mysqluser:mysqlpass@mysqldb_products:3316/product')
+
+
+const testConnection = async() => {
+
+ try {
+   await sequelize.authenticate();
+   console.log('Connection has been established successfully.');
+ } catch (error) {
+   console.error('Unable to connect to the database:', error);
+ }
+ return testConnection
+}
+
+
 
 export async function saveProduct(produto) {
   products.push(produto);
@@ -9,3 +30,5 @@ export async function findProduct() {
   const listProducts = products;
   return listProducts;
 }
+
+testConnection()
