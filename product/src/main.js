391ec0f3b2120ebd8fe1439defb60785e02b90c1
RastelliBru <bruna.rastelli@gmail.com>
2023-01-10 17:08:10 -0300

    conexão com postgresql

--- product/src/main.js
+++ product/src/main.js
@@ -2,12 +2,11 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./router.js";
 import swaggerDocs from "./api-docs.json" assert {type: "json"}
+import sequelize from "../db.js";
 
 const app = express();
 
 app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-
-
 
 app.use(express.json());
 
@@ -16,3 +15,11 @@ app.use(router);
 app.listen(3001, function () {
   console.log("running");
 });
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
