3b27c070a5427a7321161a6b6bf76efcdf6b84d5
RastelliBru <bruna.rastelli@gmail.com>
2023-01-12 10:17:19 -0300

    container product com erro Cannot read properties of undefined

--- product/models/product.js
+++ product/models/product.js
@@ -1,12 +1,22 @@
 
 import { Model, DataTypes } from 'sequelize';
 import client from '../src/repositories/dabaseclient.js';
+import { ProductFeature } from './product_feature.js';
+import { ProductImage } from './product_image.js';
 
-  class Product extends Model {
- 
-    static associate(models) {
-    }
+class Product extends Model { 
+  static associate(models) {
+   }
   }
+
+  Product.hasMany(ProductImage, {
+    foreignKey: 'product_id'
+  })
+  
+  Product.hasMany(ProductFeature, {
+    foreignKey: 'product_id'
+  })
+
   Product.init({
     id_user: DataTypes.UUID,
     name: DataTypes.STRING,
---
+++ product/models/product_image.js
@@ -0,0 +1,19 @@
+
+import { Model, DataTypes } from 'sequelize';
+import client from '../src/repositories/dabaseclient.js';
+  
+  export class ProductImage extends Model {
+    static associate(models) {
+    }
+  }
+
+  ProductImage.init({
+    url: DataTypes.STRING,
+    description: DataTypes.STRING,
+    product_id: DataTypes.INTEGER
+  }, {
+    sequelize: client,
+    modelName: 'ProductImage',
+  });
+
+  
