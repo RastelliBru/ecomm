633101cd728880d3e73e6cb10a45fb76c5dd76df
RastelliBru <bruna.rastelli@gmail.com>
2023-01-25 16:41:19 -0300

    adding salt in .env file

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -2,9 +2,10 @@ import crypto from 'crypto'
 import { saveAccount } from "../../repositories/accountRepository.js";
 import bcrypt from 'bcryptjs'
 
+
 export async function createUserUseCase(name, email, password) {
   const createdDate = new Date().toISOString().substring(0, 10);
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.HASH_SALT));
   const id = crypto.randomUUID();
   const user = {
     id,
