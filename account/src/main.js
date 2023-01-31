48b48e06c2bd5b9e4fa6e2bfcfc9b9d6a7a56918
RastelliBru <bruna.rastelli@gmail.com>
2023-01-31 15:23:19 -0300

    updating api docs

--- account/src/main.js
+++ account/src/main.js
@@ -1,8 +1,9 @@
-import swaggerUi from "swagger-ui-express";
-import apiDocs from './api-docs.json' assert {type: "json"};
+import swaggerExpress from "swagger-ui-express";
 import { app } from "./app.js";
+import yamljs from 'yamljs'
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
+const swaggerDocs = yamljs.load('docs.yaml')
+app.use("/api-docs",swaggerExpress.serve, swaggerExpress.setup(swaggerDocs))
 
 app.listen(process.env.PORT, function () {
   console.log("running");
