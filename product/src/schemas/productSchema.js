e83f230785f5366d4d18a80750d88f8d7358277c
RastelliBru <bruna.rastelli@gmail.com>
2023-02-03 00:21:37 -0300

    creating features validation in products

--- product/src/schemas/productSchema.js
+++ product/src/schemas/productSchema.js
@@ -9,6 +9,14 @@ const productSchema = Joi.object({
     quantity: Joi.number().min(1),
     description: Joi.string().min(200).max(500),
     category: Joi.string().required(),
+    images: Joi.array().min(3).required().items({
+        url: Joi.string().required(),
+        description: Joi.string().required()
+    }),
+    features: Joi.array().min(3). required().items({
+        name: Joi.string().required(),
+        description: Joi.string().required() 
+    }) 
 })
 
 const productValidate = validator(productSchema)
